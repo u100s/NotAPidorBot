1188bad80ec313779866211c67d960e234ecaccb
u100s <i@u100s.ru>
2024-09-12 01:35:46 +0300

    Add configuration for GPT model

--- Models/ChatGpt/RequestBody.cs
+++ Models/ChatGpt/RequestBody.cs
@@ -4,7 +4,7 @@ using System.Linq;
 namespace NotAPidorBot.Models.ChatGpt;
 public class RequestBody
 {
-    public string model { get; init; } = "gpt-4o";
+    public string model { get; init; } = Settings.BotConfiguration.ChatGptModel;
     public int max_tokens { get; init; } = Settings.BotConfiguration.ChatGptMaxTokens;
     public Message[] messages { get; set; }
     public int temperature { get; init; } = 1;

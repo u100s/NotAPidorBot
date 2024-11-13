d3ba8b33f23935508c6dfd95d27d3b98bb141d89
u100s <i@u100s.ru>
2024-11-13 20:29:28 +0300

    Added Created datetime to messages

--- Models/TotalContext/ChatMessage.cs
+++ Models/TotalContext/ChatMessage.cs
@@ -6,11 +6,14 @@ public class ChatMessage : MessageBase
     public bool IsAnswerFromGPT { get; private set; }
     public string ForwardedFrom { get; private set; }
 
+    public DateTime Created { get; private set; }
+
     public ChatMessage(int speakerId, long messageId, string text, bool isAnswerFromGPT, string forwardedFrom = "") : base(text)
     {
         SpeakerId = speakerId;
         MessageId = messageId;
         IsAnswerFromGPT = isAnswerFromGPT;
         ForwardedFrom = forwardedFrom;
+        Created = DateTime.Now;
     }
 }
